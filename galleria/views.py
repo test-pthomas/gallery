d7ed2b1fcebc62066acf59368e069eb0a36eed12
Peter Thomas <pjthomas404@gmail.com>
2012-04-11 21:46:42 -0500

    changed home page to list of galleries removed gallery_list view and url

--- galleria/views.py
+++ galleria/views.py
@@ -5,14 +5,9 @@ from django.core.urlresolvers import reverse
 from galleria.models import Gallery
 from galleria.forms import CreateForm
 
-
 def home(request):
-    context = RequestContext(request)
-    return render(request, 'home.html', context_instance=context)
-
-def gallery_list(request):
     context = RequestContext(request, { 'galleries': Gallery.objects.all()})
-    return render(request, 'list.html', context_instance=context)
+    return render(request, 'home.html', context_instance=context)
     
 def my_galleries(request):
     context = RequestContext(request, { 'galleries': Gallery.objects.all().filter(user=request.user)})
